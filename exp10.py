fdc6df5369ac0436d012bace62f5e7e432e8d10c
Suraj Ranjan <132946363+Suraj-Ranjan7667@users.noreply.github.com>
2025-06-11 03:05:56 +0530

    Update exp10.py

--- exp10.py
+++ exp10.py
@@ -1,48 +1,34 @@
 N = 8
 
 def printSolution(board):
-    for i in range(N):
-        for j in range(N):
-            print(board[i][j], end=" ")
-        print()
+    for row in board:
+        print(' '.join(map(str, row)))
 
 def isSafe(board, row, col):
     for i in range(col):
-        if board[row][i] == 1:
+        if board[row][i] or \
+           (row - i >= 0 and board[row - i][col - i]) or \
+           (row + i < N and board[row + i][col - i]):
             return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
     return True
 
 def solveNQUtil(board, col):
-    if col >= N:
+    if col == N:
         return True
-
     for i in range(N):
         if isSafe(board, i, col):
             board[i][col] = 1
-
             if solveNQUtil(board, col + 1):
                 return True
-
             board[i][col] = 0
-
     return False
 
 def solveNQ():
-    board = [[0] * N for _ in range(N)]
-
+    board = [[0]*N for _ in range(N)]
     if not solveNQUtil(board, 0):
-        print("Solution does not exist")
+        print("No solution")
         return False
-
     printSolution(board)
     return True
+
 solveNQ()
